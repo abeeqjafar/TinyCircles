1bec20929dd34dbe4719e70e20f7aab774d813c8
Abeeq <l164253@lhr.nu.edu.pk>
2021-05-19 10:57:34 +0500

    first commit

---
+++ Assets/gameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gameManager : MonoBehaviour
+{
+    public Text score;
+    float timer = 0.0f;
+    int start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        start = (int)timer % 60;
+        score.text =  "Score: " + start.ToString();
+       
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject other, gameManager, resetBtn;
+    public float speed = 3f;
+    bool flag;
+
+    private void Start()
+    {
+        flag = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            flag = false;
+            gameManager.SetActive(false);
+            resetBtn.SetActive(true);
+        }
+    }
+
+
+    void Update()
+    {
+        if (flag && gameManager.activeSelf)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, other.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
